9a6ed5fd19326342bf7df1d6f9ed50928d9df3db
hieule2108 <levanhieusona@gmail.com>
2024-07-13 10:51:47 +0700

    Khởi tạo dự án

---
+++ client/README.md
@@ -0,0 +1,4 @@
+# Cách chạy dự án
+
+- cd vào thư mục client
+- chạy lệnh `npm run dev`
---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className='text-red-800 font-bold'>Hello world</h1>
+}
+
+export default App

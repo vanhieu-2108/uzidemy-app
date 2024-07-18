003bd96c5fd0122b22afad2ae32e951c0328e145
hieule2108 <levanhieusona@gmail.com>
2024-07-18 12:57:37 +0700

    Implement upload image and video, Edit client react -> nextjs

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/header";
+
+export default function Home() {
+  return (
+    <main className="p-5">
+      <Header />
+      <h1>hehe</h1>
+    </main>
+  );
+}
---
+++ client/src/components/header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}

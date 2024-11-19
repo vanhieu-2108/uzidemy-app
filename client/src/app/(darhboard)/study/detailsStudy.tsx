2d1e94d7bd88941462ed406550461479a927b991
nguyenphuocvu <vunguyenphuoc2906@gmail.com>
2024-11-19 13:09:02 +0700

    update study

---
+++ client/src/app/(darhboard)/study/detailsStudy.tsx
@@ -0,0 +1,14 @@
+"user client";
+import Link from "next/link"
+import React , {useState} from 'react'
+
+const detailsStudy = () => {
+  return (
+    <div>
+       <h2>OK</h2>
+    </div>
+  )
+}
+
+export default detailsStudy
+
